9b27a5e7f06e3ceffe7d13221e0e61093f08d084
stephentetley <stephen.tetley@gmail.com>
2019-07-01 23:53:24 +0100

    Added a ComplierMonad.

---
+++ src/AssetTrafo/Base/CompilerMonad.fs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Stephen Tetley 2019
+// License: BSD 3 Clause
+
+namespace AssetTrafo.Base
+
+
+module CompilerMonad =
+
+    type ErrMsg = string
+
+    type Ans<'a, 'state> = Result<'a * 'state, ErrMsg> 
+
+    /// The compiler monad "has everything": state + reader + error.
+    /// It also has 'res is an immutable handle that cannot even be changed by 'local'
+    type CompilerMonad<'a, 'res, 'env, 'state> = 
+        | CompilerMonad of ('res -> 'env -> 'state -> Ans<'a, 'state>)
+
+    let private apply1 (ma : CompilerMonad<'ans, 'res, 'env, 'state>)
+                       (handle : 'res) 
+                       (env : 'env) 
+                       (state : 'state) : Ans<'ans, 'state> = 
+            let (CompilerMonad f) = ma in f handle env state
+
+    let private failM () : CompilerMonad<'ans, 'res, 'env, 'state> = 
+        CompilerMonad <| fun _ _ _ -> Error "failM"
+
+    let mreturn (x : 'ans) : CompilerMonad<'ans, 'res, 'env, 'state> =
+        CompilerMonad <| fun _ _ st -> Ok (x, st)
+
