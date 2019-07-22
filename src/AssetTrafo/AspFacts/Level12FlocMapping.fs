2592ee21e41b39bc521cda70feffb59f15aa73b4
stephentetley <stephen.tetley@gmail.com>
2019-07-22 15:00:56 +0100

    Added modules to generate Level 1 and 2 mappings for Clingo.

---
+++ src/AssetTrafo/AspFacts/Level12FlocMapping.fs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Stephen Tetley 2019
+// License: BSD 3 Clause
+
+namespace AssetTrafo.AspFacts
+
+
+module Level12FlocMapping =
+
+    open FSharp.Interop.Excel
+
+    open FactX
+    open FactX.FactWriter
+
+    open AssetTrafo.AspFacts.Common
+
+    // ********** DATA SETUP **********
+    type SiteMappingTable = 
+        ExcelFile< @"G:\work\Projects\asset_sync\Lvl1_2FlocMapping.xlsx",
+                    SheetName = "Mapping!",
+                    ForceString = true >
+    
+    type SiteMappingRow = SiteMappingTable.Row
+
+
+    let getSiteMappingRows (xlsxPath : string) : SiteMappingRow list = 
+        let isBlank (row : SiteMappingRow) = 
+            try 
+                // First column / cell should be an int
+                match row.AI2_InstallationReference with
+                | null | "" -> true
+                | _ -> false
+            with
+            | _ -> true
+        (new SiteMappingTable(filename = xlsxPath)).Data
+            |> Seq.filter (not << isBlank)
+            |> Seq.toList
+
+
+    let checkInput (input : string) : string option = 
+        match input with
+        | null | "NULL" -> None
+        | _ -> input.Trim() |> Some
+
+
+    // ************************************************************************
+    // Aib Common Name
+    
+    let private aibCommonName (row : SiteMappingRow) : Predicate option = 
+        let make1 = 
+            fun code name -> predicate "aib_common_name" [ stringTerm code; stringTerm name ]
+        match (checkInput row.AI2_InstallationReference, 
+                checkInput row.AI2_InstallationCommonName) with
+        | Some code, Some name -> make1 code name |> Some
+        | _, _ -> None
+
+
+    
+
+    let generateCommonNameFacts (mappingRows : SiteMappingRow list)
+                                (outputFile : string) : unit =  
+        generatePredicates aibCommonName mappingRows
+            |> writeFactsWithHeaderComment outputFile
+            
+
+
+
+    // ************************************************************************
+    // Description Lookups (code to description) (S4)
+
+    let level2Mapping (row : SiteMappingRow) : Predicate option = 
+        let make1 = 
+            fun sai floc1 s4Name -> 
+                predicate "level1_mapping" [ stringTerm sai
+                                           ; stringTerm floc1
+                                           ; stringTerm s4Name ]
+        match (checkInput row.AI2_InstallationReference, 
+                checkInput row.``S/4 Hana Floc Lvl1_Code``, 
+                checkInput row.``S/4 Hana Floc Description``) with
+        | Some code, Some floc1, Some name -> make1 code floc1 name |> Some
+        | _, _, _-> None
+
+
+    let level3Mapping (row : SiteMappingRow) : Predicate option = 
+        let make1 = 
+            fun sai floc1 floc2 -> 
+                predicate "level2_mapping" [ stringTerm sai
+                                           ; stringTerm floc1
+                                           ; stringTerm floc2 ]
+        match (checkInput row.AI2_InstallationReference, 
+                checkInput row.``S/4 Hana Floc Lvl1_Code``, 
+                checkInput row.``S/4 Hana Floc Lvl2_Code``) with
+        | Some code, Some floc1, Some name -> make1 code floc1 name |> Some
+        | _, _, _-> None
+
+    let generateLevel12Mappings (mappingRows : SiteMappingRow list)
+                                (outputFile : string) : unit =  
+        writeFactsWithHeaderComment outputFile
+            <| factWriter { 
+                    do! generatePredicates level2Mapping mappingRows
+                    do! generatePredicates level3Mapping mappingRows
+                    return ()
+                }
+
+ 
---
+++ test/Facts/RunLevel12FlocMapping.fsx
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Stephen Tetley 2019
+
+#r "netstandard"
+#r "System.Xml.Linq.dll"
+open System.IO
+
+
+#I @"C:\Users\stephen\.nuget\packages\ExcelProvider\1.0.1\lib\netstandard2.0"
+#r "ExcelProvider.Runtime.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\ExcelProvider\1.0.1\typeproviders\fsharp41\netstandard2.0"
+#r "ExcelDataReader.DataSet.dll"
+#r "ExcelDataReader.dll"
+#r "ExcelProvider.DesignTime.dll"
+open FSharp.Interop.Excel
+
+#I @"C:\Users\stephen\.nuget\packages\FParsec\1.0.4-rc3\lib\portable-net45+win8+wp8+wpa81"
+#r "FParsec"
+#r "FParsecCS"
+open FParsec
+
+#I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190721\lib\netstandard2.0"
+#r "SLFormat"
+
+#I @"C:\Users\stephen\.nuget\packages\factx\1.0.0-alpha-20190721\lib\netstandard2.0"
+#r "FactX"
+
+
+#I @"C:\Users\stephen\.nuget\packages\slpotassco\1.0.0-alpha-20190721\lib\netstandard2.0"
+#r "SLPotassco"
+open SLPotassco.Potassco.Invoke
+
+#load "..\..\src\AssetTrafo\AspFacts\Common.fs"
+#load "..\..\src\AssetTrafo\AspFacts\Level12FlocMapping.fs"
+open AssetTrafo.AspFacts.Level12FlocMapping
+
+let clingoDirectory () = 
+    System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\..\clingo")
+
+
+let outputFile (relativePath : string) = 
+    Path.Combine(__SOURCE_DIRECTORY__, @"..\..\clingo\facts", relativePath)
+
+
+let main () = 
+    let source = @"G:\work\Projects\asset_sync\Lvl1_2FlocMapping.xlsx"
+    let rows = getSiteMappingRows source
+    generateCommonNameFacts rows (outputFile "aib_common_names.lp") 
+    generateLevel12Mappings rows (outputFile "floc_mapping_1_2.lp") 
+
