365a8f7ce12f5b7b6f91faab83ce903c7e862504
stephentetley <stephen.tetley@gmail.com>
2019-09-22 21:19:15 +0100

    FSharp FlocMapping - adding scaffold code for a Giraffe web interface.

---
+++ fsharp/floc-mapping/web/Program.fs
@@ -0,0 +1,30 @@
+﻿// Currently, this is just the sample code from the Giraffe GitHub pages
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong"
+        route "/"       >=> htmlFile "../../../pages/index.html" ]
+
+let configureApp (app : IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0
