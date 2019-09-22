f975d65239910aa67a50334cdc8d9266d0d60a51
stephentetley <stephen.tetley@gmail.com>
2019-09-22 21:36:01 +0100

    Added very simple sample of using GiraffeViewEngine.

---
+++ fsharp/floc-mapping/web/Test01.fsx
@@ -0,0 +1,17 @@
+﻿#I @"C:\Users\stephen\.nuget\packages\giraffe\3.6.0\lib\netstandard2.0"
+#r "Giraffe.dll"
+
+open Giraffe.GiraffeViewEngine
+
+let myTemplate name = 
+    html [] [
+        body [] [
+            div [] [
+                str (sprintf "Hello %s!" name)
+            ]
+        ]
+    ]
+    |> renderHtmlDocument
+
+let demo01 () = 
+    myTemplate "world"
